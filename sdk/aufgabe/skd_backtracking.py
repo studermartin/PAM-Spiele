af989ab6817b6ee7ed11688c01d537732f14860c
martin.studer <martin.studer@gkd.ch>
2024-12-08 09:32:12 +0100

    .

--- sdk/aufgabe/skd_backtracking.py
+++ sdk/aufgabe/skd_backtracking.py
@@ -4,6 +4,7 @@
 UNASSIGNED=0    # UNASSIGNED für eine leere Zelle
 N=9             # N ist die Seitenlänge des Sudoku-Spielfeldes
 
+# Ein teilweise ausgefülltes Sudoku-Feld
 grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
         [5, 2, 0, 0, 0, 0, 0, 0, 0],
         [0, 8, 7, 0, 0, 0, 0, 3, 1],
